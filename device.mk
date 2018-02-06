e5cf323208b0d00aabb8def708059918aab8ea57
TheScarastic <warabhishek@gmail.com>
2018-02-06 18:33:57 +0530

    taco: update overlays
     * Move lineage specific overlays to overlay-lineage
     * Remove configs having default values
     * Refactor hardware keys and lights overlay as pers lineagesdk

--- device.mk
+++ device.mk
@@ -24,6 +24,7 @@ $(call inherit-product, vendor/yu/taco/taco-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
