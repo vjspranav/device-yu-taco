18d1914f859d717d8b05960d43023b9801c511ed
vjspranav <pranavasri@live.in>
2018-06-06 00:35:57 +0530

    Generate treble-compatible makefiles

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -39,8 +39,8 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 # Copyright headers and guards
 write_headers
 
-write_makefiles "$MY_DIR"/proprietary-files.txt 
-write_makefiles "$MY_DIR"/proprietary-files-common.txt
+write_makefiles "$MY_DIR"/proprietary-files.txt true
+write_makefiles "$MY_DIR"/proprietary-files-common.txt true
 
 cat << EOF >> "$ANDROIDMK"
 \$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
